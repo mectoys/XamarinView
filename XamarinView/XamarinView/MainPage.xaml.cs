17f996291daf5d5682adacd2012f1223105f031e
JOSE ALFREDO PONCIANO CARRANZA <mectoys2013@gmail.com>
2018-09-29 13:04:57 -0500

    Slider

--- XamarinView/XamarinView/MainPage.xaml.cs
+++ XamarinView/XamarinView/MainPage.xaml.cs
@@ -12,7 +12,20 @@ namespace XamarinView
         public MainPage()
         {
             InitializeComponent();
-            
+            dtp.Date = DateTime.Now;
+            dtp.MinimumDate = new DateTime(1990,1,1);
+            dtp.MaximumDate = new DateTime(2020,1,1);
+
+        }
+
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void redSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+
         }
     }
 }
